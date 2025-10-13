e3f57c65af260234e7e4384dcdf6cd59b55544e2
Chandu JR <chandujr@gmail.com>
2025-10-14 01:48:54 +0530

    add warning block for js blocker

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+// Checking for JS blocker
+const warningBlock = document.getElementById("warning-block");
+if (warningBlock) warningBlock.style.display = "none";
+
 // Simple scale animation function
 function addButtonAnimation(button) {
   button.classList.add("scale-down");
